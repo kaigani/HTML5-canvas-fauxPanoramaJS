a033fe578726ff4b1d8f4365eb347f1208693fc0
Kai Turner <kai@kaigani.com>
2014-03-28 08:36:37 +0000

    WIP: slice_panorama
    Adding a node script to automatically convert large images to the
    slices used by FauxPanoramaJS

---
+++ bin/graphicsmagick/README.md
@@ -0,0 +1,10 @@
+INSTALL GRAPHICSMAGICK
+===========================
+
+To use the image slicing scripts found in /resources/slice_panorama/ you must install GRAPHICSMAGICK.
+
+On OSX:
+> brew install graphicsmagick
+
+More info:
+http://www.graphicsmagick.org/
---
+++ resources/slice_panorama/test.js
@@ -0,0 +1,17 @@
+// gm - Copyright Aaron Heckmann <aaron.heckmann+github@gmail.com> (MIT Licensed)
+
+var gm = require('gm') ,
+   dir = __dirname + '/_out';
+
+gm(525, 110, "#00ff55aa")
+  .fontSize(68)
+  .stroke("#efe", 2)
+  .fill("#555")
+  .drawText(20, 72, "graphics")
+  .fill("#fa0")
+  .drawText(274, 72, " magick")
+  .write(dir + '/new.png', function(err){
+    if (err) return console.dir(arguments);
+    console.log(this.outname + ' created  :: ' + arguments[3]);
+  });
+  
